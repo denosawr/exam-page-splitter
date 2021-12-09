4e2780b5a52bf329fa855e6230d5030e32ab7216
Ollie Cheng <olliecheng@outlook.com>
2021-12-09 11:25:50 +0800

    Rename file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import sys
 from pprint import pprint
 
-import splitter
+import question_splitter
 
 # convert to command-line (click) later
 file = sys.argv[1]
-pprint(splitter.split_question(file))
+pprint(question_splitter.split_question(file))
