34803ed1c657d92339edac27bfa700fe1f2f9626
Ollie Cheng <olliecheng@outlook.com>
2021-12-09 20:52:14 +0800

    Split PDF into questions POC

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 from pprint import pprint
 
 import PyPDF2
@@ -7,6 +8,8 @@ import question_splitter
 
 # convert to command-line (click) later
 file = sys.argv[1]
+path = Path(file)
+
 results = question_splitter.split_question(file)
 
 pprint(results)
@@ -14,16 +17,21 @@ pprint(results)
 import pdf_splitter
 
 src_f = open(file, "rb")
-output = PyPDF2.PdfFileWriter()
+input_PDF = PyPDF2.PdfFileReader(file)
+
+parent = path.parent
+folder = parent / path.stem
+folder.mkdir(exist_ok=True)
+
 for question, pages in results.items():
+    output = PyPDF2.PdfFileWriter()
     for page_data in pages:
         print(question, page_data)
-        input_PDF = PyPDF2.PdfFileReader(file)
         p: PyPDF2.pdf.PageObject = input_PDF.getPage(page_data.page)
         new_page = pdf_splitter.extract_viewport(p, page_data.viewport)
 
         output.addPage(new_page)
-src_f.close()
+    with open(folder / (str(question) + ".pdf"), "wb") as f:
+        output.write(f)
 
-with open("tests/output.pdf", "wb") as f:
-    output.write(f)
+src_f.close()
