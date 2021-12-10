06c5ab8c02e71a6f91c6f21f323437dca9b28ec0
Ollie Cheng <olliecheng@outlook.com>
2021-12-10 12:07:52 +0800

    Add exam origins to generated PDF

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import io
 import sys
+from io import StringIO
 from pathlib import Path
 from pprint import pprint
 
 import PyPDF2
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
 
 import question_splitter
 
@@ -23,12 +27,25 @@ parent = path.parent
 folder = parent / path.stem
 folder.mkdir(exist_ok=True)
 
+packet = io.BytesIO()
+print(A4)
+can = canvas.Canvas(packet, pagesize=A4)
+can.drawString(55, 790, path.stem)
+can.save()
+
+packet.seek(0)
+
+new_pdf = PyPDF2.PdfFileReader(packet)
+textPage: PyPDF2.pdf.PageObject = new_pdf.getPage(0)
+
 for question, pages in results.items():
     output = PyPDF2.PdfFileWriter()
     for page_data in pages:
         print(question, page_data)
         p: PyPDF2.pdf.PageObject = input_PDF.getPage(page_data.page)
         new_page = pdf_splitter.extract_viewport(p, page_data.viewport)
+
+        new_page.mergePage(textPage)
 
         output.addPage(new_page)
     with open(folder / (str(question) + ".pdf"), "wb") as f:
